613fa728a2adf244d77b574df0da1f4d8fc6b166
deadbok <martin.groenholdt@gmail.com>
2016-08-13 14:25:25 +0200

    Updated layout

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ Config file for my personal blog.
 CONFIG = {
         'AUTHOR': 'Martin Bo Kristensen Grønholdt',
         'SITEURL': 'https://groenholdt.net',
-        'SITENAME': 'Homepage V. 5.1',
+        'SITENAME': 'Homepage V. 5.1.3',
         'POSTSPERINDEX': 6,
         'METAPARSERS': ['CategoryMetaParser'],
         'GENERATORS': ['BlogIndexGenerator', 'CategoryIndexGenerator', 'TagCloudGenerator'],
